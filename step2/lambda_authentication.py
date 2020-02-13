7e4e4d744b8a2d83db3cbc638d956be9a6e0ea85
IoT.kyoto <next@kyoso.co.jp>
2020-02-13 11:31:00 +0900

    fix: S3に画像データを登録せずにバイナリを利用する、thresholdオプション化

--- step2/lambda_authentication.py
+++ step2/lambda_authentication.py
@@ -8,8 +8,6 @@ import boto3
 from datetime import datetime
 from botocore.exceptions import ClientError
 import base64
-import string
-import random
 import uuid
 import botocore.exceptions
 
@@ -22,21 +20,22 @@ MAX_FACES = 10
 class Rekognition():
      def __init__(self):
           self.required_keys: list
+          self.optional_keys: list
           self.rekognition_client = REKOGNITION_CLIENT
           self.collection_id = COLLECTION_ID
           self.max_faces = MAX_FACES
 
-     def convert_to_float(self, data) -> float:
-          """
-          受け取ったオブジェクトをFloat型にする
-          """
-          return float(data)
+     # def convert_to_float(self, data) -> float:
+     #      """
+     #      受け取ったオブジェクトをFloat型にする
+     #      """
+     #      return float(data)
      
-     def round_to_2nd_decimal(self, data) -> float:
-          """
-          受け取った数値を小数点第三位で四捨五入する
-          """
-          return round(data, 2)
+     # def round_to_2nd_decimal(self, data) -> float:
+     #      """
+     #      受け取った数値を小数点第三位で四捨五入する
+     #      """
+     #      return round(data, 2)
 
      def make_timestamp(self) -> str: 
           """
@@ -49,48 +48,53 @@ class Rekognition():
           """
           Rekognition結果を解析し、Response Bodyのデータを作成する
           """
-          print('create_response')
+          print('[DEBUG]create_response()')
           payloads: dict = {}
           payloads['timestamp'] = self.make_timestamp()
           payloads.update(data)
           return self.make_response(200, '[SUCCEEDED]Rekognition done', payloads)
 
-     def search_face(self, s3_bucket: str, s3_obj: str, threshold: float) -> dict:
+     def decode_base64_to_binary(self, image_base64: str) -> bytes:
+          """
+          base64のデータをバイナリデータに変換する
+          """
+          print('[DEBUG]decode_base64_to_binary()')
+          return base64.b64decode(image_base64)
+
+     def search_face(self, image_binary: bytes, threshold: float = None) -> dict:
           """
           画像とマッチする人物を特定する
           """
+          print('[DEBUG]search_face()')
           try:
                search_result = REKOGNITION_CLIENT.search_faces_by_image(
                     CollectionId=self.collection_id,
                     Image={
-                         'S3Object': {
-                         'Bucket': s3_bucket,
-                         'Name': s3_obj
-                         }
+                         'Bytes': image_binary
                     },
                     MaxFaces=self.max_faces,
                     FaceMatchThreshold=threshold
                )
                return self.create_response(search_result)
 
-          # 画像に顔が認識出来ない場合
+          # # 画像内に顔を認識出来ない場合
           # except botocore.exceptions.InvalidParameterException as error:
-          #      print(error)
+          #      print('[DEBUG]: {}'.format(error))
           #      return self.make_response(421, '画像内に人物の顔を認識出来ませんでした')
           
           # # 画像サイズが制限オーバーの場合
           # except botocore.exceptions.ImageTooLargeException as error:
-          #      print(error)
+          #      print('[DEBUG]: {}'.format(error))
           #      return self.make_response(422, '画像のサイズが大きすぎます（制限:15MB）')
           
           # # 画像フォーマットが異なる場合
           # except botocore.exceptions.InvalidImageFormatException as error:
-          #      print(error)
+          #      print('[DEBUG]: {}'.format(error))
           #      return self.make_response(423, '画像のフォーマットが異なります（制限:PNGあるいはJPEG形式）')
           
           # その他エラーに対処
           except Exception as error:
-               print(error)
+               print('[DEBUG]: {}'.format(error))
                error_code = error.response['Error']['Code']
                if (error_code == 'InvalidParameterException'):
                     return self.make_response(421, '画像内に人物の顔を認識出来ませんでした')
@@ -105,24 +109,30 @@ class Rekognition():
           """
           バリデーションチェックをする関数
           """
+          print('[DEBUG]check_validation()')
+          if not body:
+               return self.make_response(400, '[FAILED]Data required')
           if not isinstance(body, dict):
                return self.make_response(400, '[FAILED]Invalid body type')
           
           errors: list = []
-          for key in self.required_keys:
-               if not key in body.keys():
-                    errors.append('key "{}" not found'.format(key))
-               if body[key] == '':
-                    errors.append('no value found for "{}"'.format(key))
-               if key == 'bucket_name':
-                    if not isinstance(body[key], str):
-                         errors.append('invalid value type: "{}"'.format(body[key]))
-               if key == 'file_name':
-                    if not isinstance(body[key], str):
-                         errors.append('invalid value type: "{}"'.format(body[key]))
-               if key == 'threshold':
-                    if not isinstance(body[key], (int, float)):
-                         errors.append('invalid value type: "{}"'.format(body[key]))
+          for key_info in self.required_keys:
+               key_name: str = key_info['name']
+               key_type: tuple = key_info['type']
+               if not key_name in body.keys():
+                    errors.append('key "{}" not found'.format(key_name))
+               elif body[key_name] == '':
+                    errors.append('no value found for "{}"'.format(key_name))
+               elif not isinstance(body[key_name], key_type):
+                    errors.append('invalid value type: "{}"'.format(key_name))
+          for key_info in self.optional_keys:
+               key_name = key_info['name']
+               key_type = key_info['type']
+               if key_name in body.keys():
+                    if not isinstance(body[key_name], key_type):
+                         errors.append('invalid value type: "{}"'.format(key_name))
+                    elif bool not in key_type and isinstance(body[key_name], bool):
+                         errors.append('invalid value type: "{}"'.format(key_name))
           if errors:
                return self.make_response(400, '[FAILED]' + ', '.join(errors))
           else: 
@@ -132,54 +142,24 @@ class Rekognition():
           """
           JSON文字列をPythonで処理できる形にする
           """
+          print('[DEBUG]load_json()')
           if str_json is None:
                return self.make_response(400, '[FAILED]Data required')
           # Lambdaテスト時にdict型で入ってくるためif分岐
           if isinstance(str_json, str):
-               # キー名がシングルクォーテーションで囲まれた場合jsonを変換できないためダブルクォーテーションに変換
-               str_json = str_json.replace('\'', '"')
-               return json.loads(str_json)
+               try:
+                    return json.loads(str_json)
+               except json.JSONDecodeError:
+                    print('[DEBUG]json decode error')
+                    return self.make_response(400, '[FAILED]json decode error')
           else:
                return str_json
-
-     def search_post(self, event):
-          self.required_keys = ['bucket_name', 'file_name', 'threshold']
-          
-          body = self.load_json(event['body'])
-          if 'statusCode' in body:
-               return body
-          validation_errors = self.check_validation(body)
-          if validation_errors:
-               return validation_errors
-          # S3へのファイル配置
-          file_name = self.put_S3_image(body['bucket_name'], body['image_b64str'])
-          
-          # 画像認識実施
-          search_result = self.search_face(body['bucket_name'], file_name, body['threshold'])
-          return search_result
-
-     def put_S3_image(self, bucket_name:str, image_b64str:str) -> str:
-          print('put_S3_image')
-          s3 = boto3.resource('s3')
-          bucket = s3.Bucket(bucket_name)
-          b64data = image_b64str.split(',')[1]
-          # print(b64data)
-          image_binary = base64.b64decode(b64data)
-          
-          # ファイル名作成
-          key = str(uuid.uuid4())
-          print(key)
-          bucket.put_object(
-              Body=image_binary,
-              Key=key
-          )
-          return key
 
      def make_response(self, status_code: int, msg: str, payloads: dict = None):
           """
           レスポンスを作成する
           """
-          print('make_response')
+          print('[DEBUG]make_response()')
           if payloads:
                body = json.dumps({'msg': msg, 'payloads': payloads})
           else:
@@ -193,17 +173,35 @@ class Rekognition():
                'body': body,
           }
 
+     def search_post(self, event):
+          """
+          （メイン処理）データをチェックし、問題がない場合はRekognitionにかける
+          """
+          self.required_keys = [{'name': 'image_base64', 'type': (str)}]
+          self.optional_keys = [{'name': 'threshold', 'type': (float, int)}]
+          
+          body = self.load_json(event['body'])
+          if 'statusCode' in body:
+               return body
+          validation_errors = self.check_validation(body)
+          if validation_errors:
+               return validation_errors
+          image_binary = self.decode_base64_to_binary(body['image_base64'])
+          # 画像認識実施
+          search_result = self.search_face(image_binary, body['threshold'])
+          return search_result
+
 #　初期化
 rekognition = Rekognition()
 
 def lambda_handler(event, _):
      """
-     /searchに対するPOSTをトリガーに実行
+     /searchに対するPOSTをトリガーに、最初に実行される関数
      """
      try:
           response_data = rekognition.search_post(event)
-          print(response_data)
+          print('[DEBUG]response data: {}'.format(response_data))
           return response_data
      except Exception as error:
-          print('[DEBUG]: {}'.format(error))
+          print('[DEBUG]500:{}'.format(error))
           return rekognition.make_response(500, '[FAILED]An error occurred : {}'.format(str(error)))
