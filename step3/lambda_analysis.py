bfccd77fc6cb288aa2babe68a29b785e81cc7923
IoT.kyoto <next@kyoso.co.jp>
2020-02-20 11:46:29 +0900

    fix: S3への画像アップロードを経由せずRekognitionが実行できるよう修正、各種refactor

--- step3/lambda_analysis.py
+++ step3/lambda_analysis.py
@@ -5,15 +5,20 @@ Method: POST
 """
 import json
 import boto3
+from typing import Union, Tuple
 from datetime import datetime
-from botocore.exceptions import ClientError
+import botocore.exceptions
+import base64
 
 # 定数
 REKOGNITION_CLIENT = boto3.client('rekognition')
-REQUIRED_KEYS: list = ['bucket_name', 'file_name']
-
+REQUIRED_KEYS: list = [{'name': 'image_base64str', 'type': (str,)}]
 
 class AnalyzeFaces():
+     def __init__(self):
+          self.rekognition_client = REKOGNITION_CLIENT
+          self.required_keys = REQUIRED_KEYS
+     
 
      def make_timestamp(self) -> str: 
           """
@@ -22,13 +27,11 @@ class AnalyzeFaces():
           date_now = datetime.now()
           return str(date_now.strftime('%Y-%m-%d %H:%M:%S'))
 
-
-     def create_response(self, data: dict):
+     def create_response(self, data: dict) -> dict:
           """
           Rekognition結果を解析し、Response Bodyのデータを作成する
           """
-
-          # Responseの作成
+          print('[DEBUG]create_response()')
           payloads: dict = {}
           payloads['timestamp'] = self.make_timestamp()
 
@@ -38,68 +41,91 @@ class AnalyzeFaces():
           else:
                return self.make_response(404, '[FAILED]No face found')
 
-     def analyze_faces(self, s3_bucket: str, s3_obj: str) -> dict:
+     def decode_base64_to_binary(self, image_base64str: str) -> bytes:
+          """
+          base64のデータをバイナリデータに変換する
+          """
+          print('[DEBUG]decode_base64_to_binary()')
+          # 画像データから不要な値を抜き出す(コンマがない場合は不要な値がないとし、全文字列を利用)
+          image_base64str = image_base64str[image_base64str.find(',') + 1 :]
+          return base64.b64decode(image_base64str)
+          
+     def analyze_faces(self, image_binary: bytes) -> dict:
           """
           画像の中から顔を検出し、その顔の分析結果を返す
           """
+          print('[DEBUG]analyze_faces()')
           try:
-               analyze_result = REKOGNITION_CLIENT.detect_faces(
+               analysis_result = self.rekognition_client.detect_faces(
                     Image={
-                         'S3Object': {
-                         'Bucket': s3_bucket,
-                         'Name': s3_obj
-                         }
+                         'Bytes': image_binary
                     },
                     Attributes=["ALL"]
                )
-               return self.create_response(analyze_result)
+               return self.create_response(analysis_result)
           
           # S3アクセス時のエラーに対処
           except Exception as error:
+               print('[DEBUG]: {}'.format(error))
                return self.make_response(400, '[FAILED]{}'.format(error))
+               # error_code = error.response['Error']['Code']
+               # if (error_code == 'InvalidParameterException'):
+               #      return self.make_response(421, '画像内に人物の顔を認識出来ませんでした')
+               # elif (error_code == 'ImageTooLargeException'):
+               #      return self.make_response(422, '画像のサイズが大きすぎます（制限:15MB）')
+               # elif (error_code == 'InvalidImageFormatException'):
+               #      return self.make_response(423, '画像のフォーマットが異なります（制限:PNGあるいはJPEG形式）')
+               # else:
+               #      return self.make_response(500, '[FAILED]{}'.format(error))
 
-
-     def check_validation(self, body: dict):
+     def check_validation(self, body: dict) -> Union[None, dict]:
           """
           バリデーションチェックをする関数
           """
+          print('[DEBUG]check_validation()')
+          if not body:
+               return self.make_response(400, '[FAILED]Data required')
           if not isinstance(body, dict):
                return self.make_response(400, '[FAILED]Invalid body type')
           
           errors: list = []
-          for key in REQUIRED_KEYS:
-               if not key in body.keys():
-                    errors.append('key "{}" not found'.format(key))
-               if body[key] == '':
-                    errors.append('no value found for "{}"'.format(key))
-               if not isinstance(body[key], str):
-                    errors.append('invalid value type: "{}"'.format(body[key]))
+          for key_info in self.required_keys:
+               key_name: str = key_info['name']
+               key_type: tuple = key_info['type']
+               if not key_name in body.keys():
+                    errors.append('key "{}" not found'.format(key_name))
+               elif body[key_name] == '':
+                    errors.append('no value found for "{}"'.format(key_name))
+               elif type(body[key_name]) not in key_type:
+                    errors.append('invalid value type: "{}"'.format(key_name))
 
           if errors:
                return self.make_response(400, '[FAILED]' + ', '.join(errors))
           else: 
-               return
+               return None
 
-
-     def load_json(self, str_json: str) -> dict:
+     def load_json(self, str_json: str) -> Tuple[bool, dict]:
           """
           JSON文字列をPythonで処理できる形にする
           """
+          print('[DEBUG]load_json()')
           if str_json is None:
-               return self.make_response(400, '[FAILED]Data required')
+               return False, self.make_response(400, '[FAILED]Data required')
           # Lambdaテスト時にdict型で入ってくるためif分岐
           if isinstance(str_json, str):
-               # キー名がシングルクォーテーションで囲まれた場合jsonを変換できないためダブルクォーテーションに変換
-               str_json = str_json.replace('\'', '"')
-               return json.loads(str_json)
+               try:
+                    return True, json.loads(str_json)
+               except json.JSONDecodeError:
+                    print('[DEBUG]json decode error')
+                    return False, self.make_response(400, '[FAILED]json decode error')
           else:
-               return str_json
+               return True, str_json
 
-
-     def make_response(self, status_code: int, msg: str, payloads: dict = None):
+     def make_response(self, status_code: int, msg: str, payloads: dict = None) -> dict:
                """
                レスポンスを作成する
                """
+               print('[DEBUG]make_response()')
                if payloads:
                     body = json.dumps({'msg': msg, 'payloads': payloads})
                else:
@@ -115,20 +141,24 @@ class AnalyzeFaces():
 #　初期化
 analyze_post = AnalyzeFaces()
 
-def lambda_handler(event, _):
+def lambda_handler(event, _) -> dict:
      """
      /analyzeに対するPOSTをトリガーに実行
      """
      try:
-
-          body = analyze_post.load_json(event['body'])
-          if 'statusCode' in body:
+          # リクエストボディの中の文字列型JSONデータをPythonで扱える形に変換する
+          result, body = analyze_post.load_json(event['body'])
+          if not result:
                return body
+          # リクエストボディの中に必要なパラメータがあるかや型が合っているかをチェックする
           validation_errors = analyze_post.check_validation(body)
           if validation_errors:
                return validation_errors
-          analyzing_result = analyze_post.analyze_faces(body['bucket_name'], body['file_name'])
+          # リクエストボディから送られた画像データをバイナリに変換しRekognitionが受け取れる形にする
+          image_binary = analyze_post.decode_base64_to_binary(body['image_base64str'])
+          # 画像認識を行い、結果を返す
+          analyzing_result = analyze_post.analyze_faces(image_binary)
           return analyzing_result
      except Exception as error:
-          print('[DEBUG]: {}'.format(error))
+          print('[DEBUG]500: {}'.format(error))
           return analyze_post.make_response(500, '[FAILED]An error occurred : {}'.format(str(error)))
