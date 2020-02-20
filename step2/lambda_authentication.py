e88b948039df9e357ca777dad585bed37c7b536e
IoT.kyoto <next@kyoso.co.jp>
2020-02-20 11:48:06 +0900

    refactor: 3章のLambdaコードに寄せる、コメント追加など

--- step2/lambda_authentication.py
+++ step2/lambda_authentication.py
@@ -5,6 +5,7 @@ Method: POST
 """
 import json
 import boto3
+from typing import Union, Tuple
 from datetime import datetime
 import botocore.exceptions
 import base64
@@ -15,13 +16,17 @@ COLLECTION_ID = 'yamada-authentication-collection'
 # Rekognitionで一度に検出したい顔の最大数（最大4096人まで可）
 MAX_FACES = 10
 
-class Rekognition():
+# リクエストボディに必須の値(name)とその型(type)の定義
+REQUIRED_KEYS: list = [{'name': 'image_base64str', 'type': (str,)}]
+OPTIONAL_KEYS: list = [{'name': 'threshold', 'type': (float, int)}]
+
+class RecognizeFaces():
      def __init__(self):
-          self.required_keys: list
-          self.optional_keys: list
           self.rekognition_client = REKOGNITION_CLIENT
           self.collection_id = COLLECTION_ID
           self.max_faces = MAX_FACES
+          self.required_keys = REQUIRED_KEYS
+          self.optional_keys = OPTIONAL_KEYS
 
      # def convert_to_float(self, data) -> float:
      #      """
@@ -42,7 +47,7 @@ class Rekognition():
           date_now = datetime.now()
           return str(date_now.strftime('%Y-%m-%d %H:%M:%S'))
 
-     def create_response(self, data: dict):
+     def create_response(self, data: dict) -> dict:
           """
           Rekognition結果を解析し、Response Bodyのデータを作成する
           """
@@ -52,12 +57,14 @@ class Rekognition():
           payloads.update(data)
           return self.make_response(200, '[SUCCEEDED]Rekognition done', payloads)
 
-     def decode_base64_to_binary(self, image_base64: str) -> bytes:
+     def decode_base64_to_binary(self, image_base64str: str) -> bytes:
           """
           base64のデータをバイナリデータに変換する
           """
           print('[DEBUG]decode_base64_to_binary()')
-          return base64.b64decode(image_base64)
+          # 画像データから不要な値を抜き出す(コンマがない場合は不要な値がないとし、全文字列を利用)
+          image_base64str = image_base64str[image_base64str.find(',') + 1 :]
+          return base64.b64decode(image_base64str)
 
      def search_face(self, image_binary: bytes, threshold: float = None) -> dict:
           """
@@ -65,7 +72,7 @@ class Rekognition():
           """
           print('[DEBUG]search_face()')
           try:
-               search_result = REKOGNITION_CLIENT.search_faces_by_image(
+               search_result = self.rekognition_client.search_faces_by_image(
                     CollectionId=self.collection_id,
                     Image={
                          'Bytes': image_binary
@@ -103,7 +110,7 @@ class Rekognition():
                else:
                     return self.make_response(500, '[FAILED]{}'.format(error))
                
-     def check_validation(self, body: dict):
+     def check_validation(self, body: dict) -> Union[None, dict]:
           """
           バリデーションチェックをする関数
           """
@@ -127,31 +134,31 @@ class Rekognition():
                key_name = key_info['name']
                key_type = key_info['type']
                if key_name in body.keys():
-                    if type(body[key_name]) not in key_type:
+                    if type(body[key_name]) not in tuple(key_type):
                          errors.append('invalid value type: "{}"'.format(key_name))
           if errors:
                return self.make_response(400, '[FAILED]' + ', '.join(errors))
           else: 
-               return
+               return None
 
-     def load_json(self, str_json: str) -> dict:
+     def load_json(self, str_json: str) -> Tuple[bool, dict]:
           """
           JSON文字列をPythonで処理できる形にする
           """
           print('[DEBUG]load_json()')
           if str_json is None:
-               return self.make_response(400, '[FAILED]Data required')
+               return False, self.make_response(400, '[FAILED]Data required')
           # Lambdaテスト時にdict型で入ってくるためif分岐
           if isinstance(str_json, str):
                try:
-                    return json.loads(str_json)
+                    return True, json.loads(str_json)
                except json.JSONDecodeError:
                     print('[DEBUG]json decode error')
-                    return self.make_response(400, '[FAILED]json decode error')
+                    return False, self.make_response(400, '[FAILED]json decode error')
           else:
-               return str_json
+               return True, str_json
 
-     def make_response(self, status_code: int, msg: str, payloads: dict = None):
+     def make_response(self, status_code: int, msg: str, payloads: dict = None) -> dict:
           """
           レスポンスを作成する
           """
@@ -160,44 +167,34 @@ class Rekognition():
                body = json.dumps({'msg': msg, 'payloads': payloads})
           else:
                body = json.dumps({'msg': msg})
-          print(body)
           return {
                'statusCode': status_code,
-               'headers': {
-                    "Access-Control-Allow-Origin" : "*",
-               },
+               'headers': {"Access-Control-Allow-Origin" : "*"},
                'body': body,
           }
 
-     def search_post(self, event):
-          """
-          （メイン処理）データをチェックし、問題がない場合はRekognitionにかける
-          """
-          self.required_keys = [{'name': 'image_base64', 'type': (str)}]
-          self.optional_keys = [{'name': 'threshold', 'type': (float, int)}]
-          
-          body = self.load_json(event['body'])
-          if 'statusCode' in body:
-               return body
-          validation_errors = self.check_validation(body)
-          if validation_errors:
-               return validation_errors
-          image_binary = self.decode_base64_to_binary(body['image_base64'])
-          # 画像認識実施
-          search_result = self.search_face(image_binary, body['threshold'])
-          return search_result
 
 #　初期化
-rekognition = Rekognition()
+search_post = RecognizeFaces()
 
-def lambda_handler(event, _):
+def lambda_handler(event, _) -> dict:
      """
-     /searchに対するPOSTをトリガーに、最初に実行される関数
+     /searchに対するPOSTをトリガーに実行
      """
      try:
-          response_data = rekognition.search_post(event)
-          print('[DEBUG]response data: {}'.format(response_data))
-          return response_data
+          # リクエストボディの中の文字列型JSONデータをPythonで扱える形に変換する
+          result, body = search_post.load_json(event['body'])
+          if not result:
+               return body
+          # リクエストボディの中に必要なパラメータがあるかや型が合っているかをチェックする
+          validation_errors = search_post.check_validation(body)
+          if validation_errors:
+               return validation_errors
+          # リクエストボディから送られた画像データをバイナリに変換しRekognitionが受け取れる形にする
+          image_binary = search_post.decode_base64_to_binary(body['image_base64str'])
+          # 画像認識を行い、結果を返す
+          search_result = search_post.search_face(image_binary, body['threshold'])
+          return search_result
      except Exception as error:
           print('[DEBUG]500:{}'.format(error))
-          return rekognition.make_response(500, '[FAILED]An error occurred : {}'.format(str(error)))
+          return search_post.make_response(500, '[FAILED]An error occurred : {}'.format(error))
